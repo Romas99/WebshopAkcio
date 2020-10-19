02a564a8ab50b192861be89fae132406c632419f
Romas99 <lexus@chello.hu>
2020-10-19 16:15:57 +0200

    Az alkalmazás elkészült.

--- src/test/java/hu/mak/Training360/WebshopAkcio/TermekTest.java
+++ src/test/java/hu/mak/Training360/WebshopAkcio/TermekTest.java
@@ -1,22 +1,39 @@
 package hu.mak.Training360.WebshopAkcio;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
+import org.junit.rules.ExpectedException;
 
 public class TermekTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void testCreate() {
+    public void testCreate1() {
         Termek termek1 = new Termek("téliszalámi",12000L,false);
-        assertThat(termek1.getNev(), equalTo("téliszalámi"));
-        assertThat(termek1.getAr(), equalTo(12000L));
-        assertThat(termek1.isMegapack(), is(false));
+        Assert.assertEquals("téliszalámi",termek1.getNev());
+        Assert.assertEquals(12000L,termek1.getAr());
+        Assert.assertFalse(termek1.isMegapack());
+
         Termek termek2 = new Termek("uborka",2800L,true);
-        assertThat(termek2.getNev(), equalTo("uborka"));
-        assertThat(termek2.getAr(), equalTo(2800L));
-        assertThat(termek2.isMegapack(), is(true));
+        Assert.assertEquals("uborka",termek2.getNev());
+        Assert.assertEquals(2800L,termek2.getAr());
+        Assert.assertTrue(termek2.isMegapack());
+
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Rossz a termék árazása!");
+        //exception.expectMessage("A termék nevének megadása kötelező!");
+        Termek termek3 = new Termek("uborka",0L,true);
+
+    }
+
+    @Test
+    public void testCreate2() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("A termék nevének megadása kötelező!");
+        Termek termek = new Termek(null,2800L,true);
+
     }
 }
